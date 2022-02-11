f37c4a021c484f69cc183befe31a2de8600f4e57
Aurélien <aurelien.kohl@efrei.net>
2022-02-11 18:33:47 +0100

    commit creation question 3 4

---
+++ KOHL_Aurelien_TP6_ST2TRD/behavioral_design_pattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace behavioral_design_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
